40f7bd9a992eaaef402e1473bccbe0b6295794df
Chris Forrest <redrider_cr@yahoo.com>
2020-07-12 19:50:35 -0400

    spinner is working just need to style app and components

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 import { axiosWithAuth } from '../utils/AxiosWithAuth';
+import styled from 'styled-components';
 
 
 class FriendsList extends React.Component {
 
     state = {
-        friends: []
+        friends: [],
+        loading: false
     };
 
 
     componentDidMount(){
-        this.getFriendsList();
-
+         this.getFriendsList();
+       
     }
 
     getFriendsList = () => {
+        this.setState({ loading: true})
         axiosWithAuth()
             .get("/friends")
             .then(res => {
                 console.log('friends list data',res)
                 this.setState({friends: res.data})
+              //  setTimeout(() => {
+                    this.setState({ loading: false});
+              //  }, 1000)
             })
             .catch(err => console.log('error fetching friends list', err));
 
@@ -28,9 +34,10 @@ class FriendsList extends React.Component {
 
 
     render() {
+        const {loading} = this.state;
         return(
             <>
-           
+           { loading && <Loader type="Circles" color="#00bfff" height={100} width={100} />}
             {this.state.friends.map(friend => {
                 return <div key={friend.id}>
                     <h3>{friend.name}</h3>
