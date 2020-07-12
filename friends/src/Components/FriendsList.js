1545ba7d9d1228d4cf03962dbfbd303c6b6df8f3
Chris Forrest <redrider_cr@yahoo.com>
2020-07-12 17:01:21 -0400

    friends list is rendering

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
+import { axiosWithAuth } from '../utils/AxiosWithAuth';
 
 class FriendsList extends React.Component {
+
+    state = {
+        friends: []
+    };
+
+
+    componentDidMount(){
+        this.getFriendsList();
+
+    }
+
+    getFriendsList = () => {
+        axiosWithAuth()
+            .get("/friends")
+            .then(res => {
+                console.log('friends list data',res)
+                this.setState({friends: res.data})
+            })
+            .catch(err => console.log('error fetching friends list', err));
+
+    }
 
 
     render() {
         return(
-            <h3>Friends List</h3>
+            <>
+            <h2>Friends List</h2>
+            {this.state.friends.map(friend => {
+                return <div key={friend.id}>
+                    <h3>{friend.name}</h3>
+                    <h4>{friend.age} </h4>
+                    <h4>{friend.email} </h4>
+                </div>
+            })}
+            </>
         )
     }
 
